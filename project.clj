c68e9238a79be5fe89af62b1e5ac97829afb3650
Filip Bielejec <fbielejec@gmail.com>
2019-01-06 18:30:06 +0100

    release version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject lein-solc "1.1.11-SNAPSHOT"
+(defproject lein-solc "1.1.12-SNAPSHOT"
   :description "lein plugin for compiling solidity contracts"
   :author "Filip Bielejec"
   :url "https://github.com/district0x/lein-solc"
